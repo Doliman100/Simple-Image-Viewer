8e906e7aead3bc119567e61c528a6b54d7d06734
Anton Dorozhkin <doliman100@gmail.com>
2022-04-05 23:30:18 +0700

    migrate to event-driven background script
    The ImageDocument is now detected by MIME type parsed by the browser. The optional Content-Type header is no longer manually parsed.
    accepts any non-xml image type

--- Simple-Image-Viewer/background.js
+++ Simple-Image-Viewer/background.js
@@ -1,44 +1,18 @@
-const tabs = new Set();
-
-function inject(tabId) {
-  chrome.tabs.insertCSS(tabId, {file: 'style.css', runAt: 'document_start'});
-  chrome.tabs.executeScript(tabId, {file: 'content.js', runAt: 'document_end'});
-}
-
-function onHeadersReceived(details) {
-  if (!tabs.has(details.tabId)) {
-    details.responseHeaders.some(function(header) {
-      if (header.name.toLowerCase() === 'content-type' && /^image\/(jpeg|png|gif|(x-|vnd\.microsoft\.)icon|bmp|webp)$/.test(header.value)) {
-        tabs.add(details.tabId);
-
-        return true;
-      }
-    });
-  }
-}
-
-function onTabUpdated(_, changeInfo, tab) {
-  if (/^file:\/\/\/.+\.(jpe?g|png|gif|ico|bmp|webp|jfif|pjp(eg)?)$/.test(tab.url) && changeInfo.status === 'loading' || tabs.has(tab.id)) {
-    // console.log(changeInfo, tab);
-
-    inject(tab.id);
-
-    tabs.delete(tab.id);
-  }
-}
-
-chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, {types: ['main_frame'], urls: ['http://*/*', 'https://*/*']}, ['responseHeaders']);
-
-chrome.tabs.onUpdated.addListener(onTabUpdated);
+'use strict';
 
 chrome.browserAction.onClicked.addListener(() => chrome.tabs.create({url: `chrome://extensions/?id=${chrome.runtime.id}`}));
 
-chrome.extension.isAllowedFileSchemeAccess((hasAccess) => {
-  if (hasAccess) {
-    chrome.browserAction.disable();
-  } else {
-    chrome.browserAction.setTitle({title: 'No access to file URLs'});
-    chrome.browserAction.setBadgeBackgroundColor({color: '#fbbc04'});
-    chrome.browserAction.setBadgeText({text: '!'});
-  }
-});
+function init() {
+  chrome.extension.isAllowedFileSchemeAccess((hasAccess) => {
+    if (hasAccess) {
+      chrome.browserAction.disable();
+    } else {
+      chrome.browserAction.setTitle({title: 'No access to file URLs'});
+      chrome.browserAction.setBadgeBackgroundColor({color: '#fbbc04'});
+      chrome.browserAction.setBadgeText({text: '!'});
+    }
+  });
+}
+chrome.runtime.onInstalled.addListener(init);
+chrome.runtime.onStartup.addListener(init);
+chrome.management.onEnabled.addListener(init);
